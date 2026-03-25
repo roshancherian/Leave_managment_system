4d22bbf2d8ec4d6474cef27f7709bf2878d09daa
Roshan Varghese Cherian <roshancherian04@gmail.com>
2026-03-25 12:02:06 +0530

    new frontend

---
+++ FrontEnd/leave-frontend/src/components/Register.jsx
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import API from "../services/api";
+
+function Register() {
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleRegister = async () => {
+        try {
+            const res = await API.post("/auth/register", {
+                name,
+                email,
+                password,
+            });
+
+            alert("Registered Successfully");
+        } catch (err) {
+            alert("Error in Register");
+        }
+    };
+
+    return (
+        <div>
+            <h2>Register</h2>
+
+            <input placeholder="Name" onChange={(e) => setName(e.target.value)} />
+            <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+            <input placeholder="Password" type="password"
+                onChange={(e) => setPassword(e.target.value)} />
+
+            <button onClick={handleRegister}>Register</button>
+        </div>
+    );
+}
+
+export default Register;
